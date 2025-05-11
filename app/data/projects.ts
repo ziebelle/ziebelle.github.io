35d1e712cd3dcf68e5d5e06bd21dfa6a6ecaa7eb
ziebelle <ziebelle@users.noreply.github.com>
2025-05-11 23:07:36 +0200

    Update projects.ts

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -78,15 +78,16 @@ export const allProjects: Project[] = [
         image: '/images/nuxt_code.png',
         tags: ['React', 'Vue', 'Web Development'],
         links: [{ title: 'learning-vue', url: 'https://ziebelle.github.io/learning-vue/' }],
-    },
-    {
-        id: 8,
-        title: 'Math learning projects',
-        description: 'Latex writedowns',
-        image: '/images/question.png',
-        tags: ['Minimax lowerbounds', 'Mathematics', 'Research'],
-        links: [{ title: 'Minimax lowerbounds', url: 'https://github.com/ziebelle/Minimax-lowerbounds' }],
     }
+    // },
+    // {
+    //     id: 8,
+    //     title: 'Math learning projects',
+    //     description: 'Latex writedowns',
+    //     image: '/images/question.png',
+    //     tags: ['Minimax lowerbounds', 'Mathematics', 'Research'],
+    //     links: [{ title: 'Minimax lowerbounds', url: 'https://github.com/ziebelle/Minimax-lowerbounds' }],
+    // }
 ];
 
 // Helper function to get featured projects
